746bbe90c0936f67e756998a26b659ab828ab2ab
Julian Degutis de Freitas Garcia <juliang@kyros.com.br>
2017-12-18 09:59:00 -0200

    Start

---
+++ src/main/java/br/com/xpto/xptonotification/XptoNotificationApplication.java
@@ -0,0 +1,12 @@
+package br.com.xpto.xptonotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XptoNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XptoNotificationApplication.class, args);
+	}
+}
